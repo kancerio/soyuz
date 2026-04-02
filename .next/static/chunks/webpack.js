254e3b0d5c729328d1b67753c3dc31939c893322
Kancerio <babajga.rulit@gmail.com>
2026-04-02 19:46:08 +0300

    Добавлена страница настроек src\components\settings\SettingsForm.tsx

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b7c6568d21a114b4"; }
+/******/ 		__webpack_require__.h = function() { return "cfb202b8cc10af60"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
